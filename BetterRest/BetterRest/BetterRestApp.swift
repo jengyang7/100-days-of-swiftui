0019e8c76524a0cefa940642bb51741fbf7a3bac
jengyang07 <jengyang1307@gmail.com>
2023-05-21 13:09:14 +0800

    Added Project 4 - BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jayden Kong on 14/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
